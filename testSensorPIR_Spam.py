d0b419f430b2dc3c3b3319e82706ce22e7cf6214
Imam Muis <imuis373@gmail.com>
2021-08-22 21:02:20 +0700

    Notif PIR Spamming

--- testSensorPIR_Spam.py
+++ testSensorPIR_Spam.py
@@ -8,17 +8,21 @@ GPIO.setup(pin_sensorPIR, GPIO.IN)
 try:
     print("Menyiapkan Sensor PIR...") 
     time.sleep(0.5)
+
     while GPIO.input(pin_sensorPIR) == 1:
         print("Sensor PIR belum siap")
         time.sleep(0.5)
+
     print("Sensor PIR Siap!\n")
     time.sleep(0.5)
     
     while True:
         kondisi = GPIO.input(pin_sensorPIR)
+        
         if kondisi == 1:
             print("Pergerakan Terdeteksi!")
             time.sleep(1)
+
         else:
             print("Tidak ada pergerakan")
             time.sleep(1)
