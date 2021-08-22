2cc99b6bdf3624b3575c7812de2da3a9240dc84e
Imam Muis <imuis373@gmail.com>
2021-08-22 21:00:07 +0700

    Test Rotasi Motor Servo

--- testMotorServo.py
+++ testMotorServo.py
@@ -11,21 +11,26 @@ def servoMove(channel = 0, start = 10, stop = 170, speed = 1):
     print("Move from", start, "to", stop)
     print("")
     speed = float(0.02 / speed)
+
     for x in range(90, stop):
         kit.servo[channel].angle = x
         time.sleep(speed)
+
     for x in range(stop, start, -1):
         kit.servo[channel].angle = x
         time.sleep(speed)
+
     for x in range(start, 90):
         kit.servo[channel].angle = x
         time.sleep(speed)
+
     time.sleep(0.5)
     
 try: 
     while True:
         servoMove(0, 20, 160, 1)
         time.sleep(2)
+        
         servoMove(1, 20, 160, 1)
         time.sleep(2)
 
