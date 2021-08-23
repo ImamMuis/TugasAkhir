53db55852f94411ab350a73ac3360e57661aec67
Imam Muis <imuis373@gmail.com>
2021-08-23 08:13:52 +0700

    Corat coret fungsi

--- getLastNum.py
+++ getLastNum.py
@@ -1,29 +1,72 @@
 import time
 import datetime
 
-Time = 0
-def lastdigits(num):
-	# num %= 10
-	print(round(abs(num % 10), 3))
+# Time = 0
+# def lastdigits(num):
+# 	# num %= 10
+# 	print(round(abs(num)%10, 3))
 
-def timeNow():
-	now = datetime.datetime.now()
-	# FORMAT: YY.MM.DD.H:M:S
-	currentDateTime = int(now.strftime("%S"))
+# def timeNow():
+# 	now = datetime.datetime.now()
+# 	# FORMAT: YY.MM.DD.H:M:S
+# 	currentDateTime = int(now.strftime("%S"))
 
-	return currentDateTime
-	# print(currentDateTime)
+# 	return currentDateTime
+# 	# print(currentDateTime)
 
-def timeNow2():
-	now = str(datetime.datetime.now())
-	now = round(float(now.split(":")[-1]), 3)
+# def timeNow2():
+# 	now = str(datetime.datetime.now())
+# 	now = round(float(now.split(":")[-1]), 3)
 
-	return now
+# 	return now
 
-list1 = [1, 0, 1, 0, 1]
-list2 = [1, 0, 1, 0, 1]
+# class getCurrent:
+#     """docstring for get"""
+# 	def __init__(self):
+# 	    now = str(datetime.datetime.now())
+# 		self.now = now
 
-if (list1 == list2):
-	print("YES!")
+# 	def Date(self):
+# 		DATE = now.strftime("%Y-%m-%d %H:%M:%S")
+# 		self.DATE = DATE
 
-# lastdigits(123.456)
+# 		return 
+
+# 	def Time(self):
+# 	    TIME = round(float(now.split(":")[-1]), 3)
+# 		self.TIME = TIME
+# 		return 
+
+def getCurrent(data):
+    now = datetime.datetime.now()
+    if data == "Date":
+	    value = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    elif data == "Time":
+	    value = round(float(str(now).split(":")[-1]), 3)
+
+    else:
+        print("WRONG PARAMETER!")
+
+    return value
+
+tanggal = getCurrent("Date")
+waktu = getCurrent("Time")
+
+print(tanggal)
+print(waktu)
+
+# waktu = getCurrent()
+
+# print(waktu.DATE())
+# print(waktu.TIME())
+
+# list1 = [1, 0, 1, 0, 1]
+# list2 = [1, 0, 1, 0, 1]
+
+
+
+# if (list1 == list2):
+# 	print("YES!")
+
+# lastdigits(-123.456)
