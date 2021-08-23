ea037fdc5e35f9cfabbf216750c2171e3563312b
Imam Muis <imuis373@gmail.com>
2021-08-23 23:07:32 +0700

    Kirim detectedface cm bisa ke tertentu

--- SendPhotoToTelegramBot.py
+++ SendPhotoToTelegramBot.py
@@ -14,6 +14,7 @@ tokenBot = '1461219516:AAHcyhA_4NIdF5uNQrDIkhsQ0nTpaT_rjZo'
 cascadePath = 'haarcascade_frontalface_default.xml'
 faceDetector = cv2.CascadeClassifier(cascadePath)
 
+frame = 0
 count = 0
 faceState1 = False
 faceState2 = False
@@ -21,11 +22,13 @@ TimeBetween = 0
 DetectedFace_Last =  0
 notDetectedTime_Now = 0
 notDetectedTime_Last = 0
+chat_id = 0
+command = ""
 
 def Selisih(current, prev):
 	num = current - prev
 	if num < 0:
-	    num += 60
+		num += 60
 	num = round(num, 3)
 
 	return num
@@ -45,100 +48,135 @@ def getCurrent(data):
 
 
 def action(msg):
+	global chat_id
+	global command
+
 	chat_id = msg['chat']['id']
 	command = msg['text']
+
 	print('Received: %s' %command)
 
 	if command == '/start':
-		telegram_bot.sendMessage(chat_id, str("Hi Imam!"))
+		# bot.sendMessage(chat_id, str("Hi Imam!"))
+		show_keyboard = {'keyboard':[	
+										['Ambil Foto','Foto Terakhir'], 
+										['null','Waktu Sekarang']
+									]}
+		bot.sendMessage(chat_id, 'Pilih perintah:', reply_markup=show_keyboard)
 
-	elif command == '/time':
+	elif command == 'Waktu Sekarang':
 		now = datetime.datetime.now()
 		value1 = now.strftime("Time: %H:%M:%S\n")
 		value2 = now.strftime("Day : %a, %d - %b - %Y\n")
 
-		telegram_bot.sendMessage(chat_id, str(value1)+str(value2))
+		bot.sendMessage(chat_id, str(value1)+str(value2))
 
-	elif command == '/lastPhoto':
+
+	elif command == 'Foto Terakhir':
 		lastImage = os.listdir(userDir)
 		foto = lastImage[-1]
 		file = str(userDir) + '/' + str(foto)
-		telegram_bot.sendPhoto(chat_id, photo=open(file, 'rb'))
+		bot.sendPhoto(chat_id, photo=open(file, 'rb'))
+
+	elif command == 'Ambil Foto':
+		waktu = getCurrent("Date")
+		namaFile = 'DetectedFace.' + str(waktu) + '.jpg'
+		cv2.imwrite(userDir + '/' + namaFile, frame)
+
+		lastImage = os.listdir(userDir)
+		foto = lastImage[-1]
+		file = str(userDir) + '/' + str(foto)
+		bot.sendPhoto(chat_id, photo=open(file, 'rb'))
 
 	else:
-		telegram_bot.sendMessage(chat_id, str("Input Salah!"))
+		bot.sendMessage(chat_id, str("Input belum tersedia!"))
 
 def detectFace():
-    global count
-    global faceState1
-    global faceState2
-    global TimeBetween
-    global DetectedFace_Last
-    global notDetectedTime_Now
-    global notDetectedTime_Last
-    global DetectedFace_Tolerance
+	global count
+	global frame
+	global faceState1
+	global faceState2
+	global TimeBetween
+	global DetectedFace_Last
+	global notDetectedTime_Now
+	global notDetectedTime_Last
+	global DetectedFace_Tolerance
+	global command
+	jumlahWajah = 0
 
-    jumlahWajah = 0
+	succes, frame = cam.read()
+	frame = cv2.flip(frame, 1)
+	abuAbu = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = faceDetector.detectMultiScale(abuAbu, 1.3 , 5)
 
-    succes, frame = cam.read()
-    frame = cv2.flip(frame, 1)
-    abuAbu = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceDetector.detectMultiScale(abuAbu, 1.3 , 5)
+	for x, y, w, h in faces:
+		frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		jumlahWajah = int(str(faces.shape[0]))
 
-    for x, y, w, h in faces:
-        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        jumlahWajah = int(str(faces.shape[0]))
+	DetectedFace_Now = jumlahWajah
 
-    DetectedFace_Now = jumlahWajah
+	if DetectedFace_Last == 0 and DetectedFace_Now == 1:
+		DetectedFace_Last = DetectedFace_Now
+		DetectedTime_Now = getCurrent("Time")
+		TimeBetween = Selisih(DetectedTime_Now, notDetectedTime_Now)
 
-    if DetectedFace_Last == 0 and DetectedFace_Now == 1:
-        DetectedFace_Last = DetectedFace_Now
-        DetectedTime_Now = getCurrent("Time")
-        TimeBetween = Selisih(DetectedTime_Now, notDetectedTime_Now)
-        
-        # print(TimeBetween)
+		# print(TimeBetween)
 
-        if TimeBetween > DetectedFace_Tolerance or count == 0:
-            print("Wajah Terdeteksi!\n")
-            count += 1
-            faceState1 = True
-            faceState2 = True
-            print("Counter :", count)
-            print("Tanggal :", getCurrent("Date"))
-            print("")
+		if TimeBetween > DetectedFace_Tolerance or count == 0:
+			print("Wajah Terdeteksi!\n")
+			count += 1
+			faceState1 = True
+			faceState2 = True
+			print("Counter :", count)
+			print("Tanggal :", getCurrent("Date"))
+			print("")
 
-    elif DetectedFace_Last == 1 and DetectedFace_Now == 0:
-        DetectedFace_Last = DetectedFace_Now
-        notDetectedTime_Now = getCurrent("Time")
+			waktu = getCurrent("Date")
+			namaFile = 'DetectedFace.' + str(waktu) + '.jpg'
+			cv2.imwrite(userDir + '/' + namaFile, frame)
 
-    elif DetectedFace_Last == 0 and DetectedFace_Now == 0:
+			lastImage = os.listdir(userDir)
+			foto = lastImage[-1]
+			file = str(userDir) + '/' + str(foto)
+			bot.sendPhoto(1338050139, photo=open(file, 'rb'))
+			# command = 'Foto Terakhir'
 
-        if faceState1 == True:
-            notDetectedTime_Now = getCurrent("Time")
-            faceState1 = False
+	elif DetectedFace_Last == 1 and DetectedFace_Now == 0:
+		DetectedFace_Last = DetectedFace_Now
+		notDetectedTime_Now = getCurrent("Time")
 
-        if count != 0 and faceState2 == True:
-            notDetectedTime_Last = getCurrent("Time")
-            TimeBetween = Selisih(notDetectedTime_Last, notDetectedTime_Now)
+	elif DetectedFace_Last == 0 and DetectedFace_Now == 0:
 
-        # print(TimeBetween)
+		if faceState1 == True:
+			notDetectedTime_Now = getCurrent("Time")
+			faceState1 = False
 
-        if TimeBetween > DetectedFace_Tolerance and faceState2 == True:
-            print("Tidak Ada Wajah!\n")
-            faceState2 = False
+		if count != 0 and faceState2 == True:
+			notDetectedTime_Last = getCurrent("Time")
+			TimeBetween = Selisih(notDetectedTime_Last, notDetectedTime_Now)
 
-    cv2.imshow('Face Detection', frame)
+		# print(TimeBetween)
+
+		if TimeBetween > DetectedFace_Tolerance and faceState2 == True:
+			print("Tidak Ada Wajah!\n")
+			faceState2 = False
+
+	cv2.imshow('Face Detection', frame)
 
 
-telegram_bot = telepot.Bot(tokenBot)
-telegram_bot.message_loop(action)
+bot = telepot.Bot(tokenBot)
+bot.message_loop(action)
 print ('Up and Running....')
 
-while True:
-	detectFace()
+try:
+	while True:
+		detectFace()
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
 
-cam.release()
-cv2.destroyAllWindows()
+	cam.release()
+	cv2.destroyAllWindows()
+
+except KeyboardInterrupt:
+	sys.exit(0)
