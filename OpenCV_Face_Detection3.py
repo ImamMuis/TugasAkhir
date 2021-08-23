9958b9dac58aa7281708fcdece1eaefdd513d030
Imam Muis <imuis373@gmail.com>
2021-08-23 11:56:39 +0700

    Stop increment timeBetween

--- OpenCV_Face_Detection3.py
+++ OpenCV_Face_Detection3.py
@@ -10,7 +10,7 @@ faceDetector = cv2.CascadeClassifier(cascadePath)
 count = 0
 faceState1 = False
 faceState2 = False
-
+TimeBetween = 0
 DetectedFace_Last =  0
 notDetectedTime_Now = 0
 notDetectedTime_Last = 0
@@ -39,15 +39,15 @@ def faceResult():
     global DetectedFace_Last
     global notDetectedTime_Now
     global notDetectedTime_Last
-
-    DetectedFace_Tolerance = 5
+    global DetectedFace_Tolerance
+    global TimeBetween
     DetectedFace_Now = detectFace()
 
     if DetectedFace_Last == 0 and DetectedFace_Now == 1:
         DetectedFace_Last = DetectedFace_Now
         DetectedTime_Now = getCurrent("Time")
         TimeBetween = Selisih(DetectedTime_Now, notDetectedTime_Now)
-        
+        print(TimeBetween)
         if TimeBetween > DetectedFace_Tolerance or count == 0:
             print("Wajah Terdeteksi!\n")
             count += 1
@@ -67,9 +67,11 @@ def faceResult():
             notDetectedTime_Now = getCurrent("Time")
             faceState1 = False
 
-        notDetectedTime_Last = getCurrent("Time")
-        TimeBetween = Selisih(notDetectedTime_Last, notDetectedTime_Now)
+        if count != 0 and faceState2 == True:
+            notDetectedTime_Last = getCurrent("Time")
+            TimeBetween = Selisih(notDetectedTime_Last, notDetectedTime_Now)
 
+        print(TimeBetween)
         if TimeBetween > DetectedFace_Tolerance and faceState2 == True:
             print("Tidak Ada Wajah!\n")
             faceState2 = False
@@ -89,7 +91,9 @@ def getCurrent(data):
 
 def Selisih(current, prev):
     num = current - prev
-    num = round( abs(num) % 10, 3)
+    if num < 0:
+        num += 60
+    num = round(num, 3)
 
     return num
 
