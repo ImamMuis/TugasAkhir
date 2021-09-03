ab11a4a5f441c883546ff1bb6a555d6e663c9539
Imam Muis <imuis373@gmail.com>
2021-09-03 16:27:48 +0700

    Bismillah program terstuktur

--- ProgramTA/TargetProgramUtama.py
+++ ProgramTA/TargetProgramUtama.py
@@ -24,22 +24,18 @@ try:
     sp.Solenoid.kunci()
     
     while True:
-        jumlahUser = input("Jumlah User di ruangan: ")
-        if jumlahUser < maxUser:
-            user = input("Wajah dikenali? y/n: ")
-            if user == 'y':
-                sp.Solenoid.buka()
-                sp.Pintu.buka()
-                sp.Pintu.tunggu(waktuTunggu)
-                if sp.PIR.userMasuk() == True:
-                    sp.Tele.notifMasuk()
-                sp.Pintu.tutup()
-                sp.Solenoid.kunci()
-            else:    
-                sk.Webcam.foto()
-                sp.Tele.tidakDikenal()
-        else:
-            print("RUANGAN PENUH!")
+        user = input("Wajah dikenali? y/n: ")
+        if user == 'y':
+            sp.Solenoid.buka()
+            sp.Pintu.buka()
+            sp.Pintu.tunggu(waktuTunggu)
+            if sp.PIR.userMasuk() == True:
+                sp.Tele.notifMasuk()
+            sp.Pintu.tutup()
+            sp.Solenoid.kunci()
+        else:    
+            sk.Webcam.foto()
+            sp.Tele.tidakDikenal()
 
 except KeyboardInterrupt:
     print("\nProgram Stop")
