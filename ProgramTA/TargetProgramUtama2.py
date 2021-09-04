e90abbbfe16c02b77ce5025fd04940dc634ed5af
Imam Muis <imuis373@gmail.com>
2021-09-04 08:32:24 +0700

    coba

--- ProgramTA/TargetProgramUtama2.py
+++ ProgramTA/TargetProgramUtama2.py
@@ -1,18 +1,23 @@
+import time
 import RPi.GPIO as GPIO
 import sistemPintu2 as sp
 
+GPIO.setmode(GPIO.BCM)
+
 sp.PIR(17)
 sp.Solenoid(14)
-sp.L_Switch(22, 27)
-sp.driverMotor(15, 18)
-sp.LED_Indicator(9, 10, 11)
+# sp.L_Switch(22, 27)
+# sp.driverMotor(15, 18)
+# sp.LED_Indicator(9, 10, 11)
 
 try:  
-    sp.PIR.setup()
-    sp.Solenoid.kunci()
+    # sp.PIR.setup()
     
     while True:       
-        user = input("Wajah dikenali? y/n: ")
+        sp.PIR.userMasuk()
+        time.sleep(1) 
+        sp.PIR.userMasuk()
+        time.sleep(1)
 
 except KeyboardInterrupt:
     print("Program Stop")
---
+++ ProgramTA/sistemPintu2.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+class PIR:
+    def __init__(self, pinPIR):
+        self.pinPIR = pinPIR
+        GPIO.setup(self.pinPIR, GPIO.IN)
+        print('PIR di pin', self.pinPIR)
+        
+    def userMasuk(self):
+        print('PIR Mendeteksi')
+        GPIO.input(self.pinPIR)
+
+class L_Switch:
+    def __init__(self, pinBuka, pinTutup):
+        self.pinBuka = pinBuka
+        self.pinTutup = pinTutup
+        GPIO.setup(self.pinBuka, GPIO.IN)
+        GPIO.setup(self.pinTutup, GPIO.IN)
+        print('LSBuka di pin', self.pinBuka)
+        print('LSTutup di pin', self.pinTutup)
+            
+    def LS_Buka(self):
+        GPIO.input(self.pinBuka)
+        print('Pintu terbuka')
+    
+    def LS_Tutup(self):
+        GPIO.input(self.pinTutup)
+        print('Pintu tertutup')
+        
+class Solenoid:
+    def __init__(self, pinSol):
+        self.pinSol = pinSol
+        GPIO.setup(self.pinSol, GPIO.OUT)
+        print('Solenoid di pin', self.pinSol)
+    
+    def buka(self):
+        GPIO.output(self.pinSol, 1)
+        print('Solenoid Buka')
+
+    def kunci(self):
+        GPIO.output(self.pinSol, 0)
+        print('Solenoid Tutup')
