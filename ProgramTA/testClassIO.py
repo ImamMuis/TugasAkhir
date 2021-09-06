b6fe52545e9b703bf24ee48e4d38e9fba2e4b14a
Imam Muis <imuis373@gmail.com>
2021-09-06 21:52:24 +0700

    Dev pasca sidang

--- ProgramTA/testClassIO.py
+++ ProgramTA/testClassIO.py
@@ -2,13 +2,20 @@ import InputOutput as IO
 import RPi.GPIO as GPIO
 import time
 
-Selenoid = IO.Output(11)
+Selenoid = IO.Output(14)
+SensorPIR = sp.PIN.Input(17)
+LS_Buka = sp.PIN.Input(22)
+LS_Tutup = sp.PIN.Input(27)
+
+Motor1 = sp.PIN.Output(15)
+Motor2 = sp.PIN.Output(18)
+Solenoid = sp.PIN.Output(14)
 
 try:
     while True:
-        Selenoid.buka()
-        time.sleep(1)
         Selenoid.kunci()
+        time.sleep(1)
+        Selenoid.buka()
         time.sleep(1)
 
 except KeyboardInterrupt:
@@ -18,4 +25,4 @@ except:
     print("Other Error or exception occured!")
     
 finally:
-    GPIO.cleanup()
+    IO.clean()
