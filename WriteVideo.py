baab7b472c984f95c5fa19b2b93dc2c8f1741cd4
Imam Muis <imuis373@gmail.com>
2021-10-06 03:14:21 +0700

    Dasar record video

--- WriteVideo.py
+++ WriteVideo.py
@@ -1,15 +1,8 @@
-
 import cv2
 
 cam = cv2.VideoCapture(0)
 videoCodec = cv2.VideoWriter_fourcc(*'avc1')
-file = str(userDir) + '/' + str(foto)
 videoOut = cv2.VideoWriter('videoOutput.mp4', videoCodec, 25.0, (640, 480))
-
-def saveImage(img):
-    waktu = getCurrent("Date")
-    namaFile = 'Snapshot.' + str(waktu) + '.jpg'
-    cv2.imwrite(userDir + '/' + namaFile, img)
 
 while True:
     succes, frame = cam.read()
